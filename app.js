810a17cfbcd675f609d6eadf725e078c54a0e636
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-10-05 09:59:37 -0300

    Alterando o método .send() para .render() do express renderizar a página dinânica do EJS

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express(); //Retorna um objeto que contém a API do Express, responsável pelas requisições HTTP e etc.
 
+//.set() serve para definirmos variáveis para dentro do Express que passem por todo o sistema
+app.set("view engine", "ejs");
+
 // .get("url",callback) trata requisições que usam o method HTTP GET
 app.get("/produtos", function(res,resp){
-    resp.send("<html><body><h1>Listando produtos</h1></body></html>");
+    //.render(url_arquivo_ejs) renderiza um arquivo de EJS no caminho produtos/lista
+    //Obs: o EJS sempre busca os arquivos numa pasta default "views" na raiz do app
+    resp.render("produtos/lista");
 });
 
 // listen(porta, fn callback)
