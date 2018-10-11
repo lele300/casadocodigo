08276ac23c1c1ef32212bf6399c084a6c7f954e4
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-10-11 17:22:21 -0300

    Aplicanco o express-validator na config do Express | Validando dados do form | Criando DIV no HTML caso haja erro no form

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,4 +1,4 @@
-//Módulo que carrega as rotas (sendo carregado pelo auto-load em express.js)
+//Módulo que carrell as rotas (sendo carregado pelo auto-load em express.js)
 module.exports = (app) => {
     app.get("/produtos", (req, resp) => {
         const connection = app.infra.connectionFactory(); //Esse é o caminho que está o arquivo connectionFactory.js que retorna a function createDBConnection().
@@ -19,17 +19,24 @@ module.exports = (app) => {
     });
 
     app.get("/produtos/form", (req, resp) => {
-        resp.render("produtos/form");
+        resp.render("produtos/form", { erros : {}, produto : {} });
     });
 
     // Podemos usar a mesma URL para os serviços de produtos. O que os diferencia são os verbos HTTP utilizados
     app.post("/produtos", (req, resp) => {
         const produto = req.body; // Recuperando os dados do form graças ao body-parser. Retorna em JSON.
+        req.assert("titulo","Título é necessário").notEmpty();         // .assert(campo,mensagem) recebe o campo para validar e a mensagem associada.
+        req.assert("preco", "Formato inválido").isFloat();
+        const erros = req.validationErrors(); // Retorna os erros em uma lista de objetos
+        if(erros) {
+            resp.render('produtos/form',{ erros, produto });
+            return;
+        }
         const connection = app.infra.connectionFactory(); //Instanciando objeto Connection do MySQL
         const produtosDAO = new app.infra.ProdutosDAO(connection); //Instanciando a classe ProdutosDAO, passando o objeto Connection para o módulo de ProdutosDAO.js
         produtosDAO.salva(produto, (err, resultados) => {
             resp.redirect("/produtos");
         });
         connection.end();
-    });
+    }); 
 }
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const load = require("express-load");
 const bodyParse = require("body-parser"); // Extrai os dados do corpo da requisição e joga na propriedade req.body
+const expressValidator = require("express-validator");
 
 module.exports = () => {
     const app = express();
@@ -11,6 +12,8 @@ module.exports = () => {
     app.use(bodyParse.urlencoded({extended : true})); // .use() recebe funções que serão aplicadas ao request.
     // extended : true quer dizer pode entender forms mais complexos
     app.use(bodyParse.json()); // pode usar o formato JSON para envio de dados.
+    
+    app.use(expressValidator()); //Adiciona algumas funções na classe Request para validações de dados.
 
     load("routes", {cwd: 'app'}) // Carrega os módulos da pasta "routes" somente da pasta 'app' auto.
         .then("infra") // Carrega os módulos da pasta "infra" auto.
