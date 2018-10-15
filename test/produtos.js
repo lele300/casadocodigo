adbdacc52ce7bc3ae4119877686b09671a343a06
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-10-15 17:30:47 -0300

    Refatorando código para usar lib SuperTest para criação dos testes automatizados

--- test/produtos.js
+++ test/produtos.js
@@ -1,23 +1,11 @@
-const http = require("http");
-const assert = require("assert"); //Importando Biblioteca de Testes
+const express = require("../config/express")();
+const request = require("supertest")(express); //Lib de testes | Passando as configs do servidor Express
 describe("#ProdutosController", () => {
     it("#Listagem JSON", done => {
-        const configuracoes = {
-            hostname: 'localhost',
-            port:3000,
-            path:'/produtos',
-            headers: {
-                'Accept' : 'application/json'
-            }
-        };
-        
-        http.get(configuracoes, res => {
-            // .equal(valorAVerificar,resultadoEsperado);
-            assert.equal(res.statusCode, 200);
-            assert.equal(res.headers["content-type"], 'application/json; charset=utf-8')
-            done(); // Utilizado para executar essa função de callback antes de ir para o próxima instrução
-            // Sem essa função, a requisição .get() inicia uma execução assíncrona e parte para o próximo bloco
-            // que marca o fim da função it() e sai da função sem ter terminado de executar o http.get().
-        });
+        request.get("/produtos") // Faz uma requisição para "/produtos"
+            .set("Accept","application/json") // Seta a resposta como application/json
+            .expect("Content-Type",/json/) // O conteúdo do request tem que ter JSON. Usado por Regex
+            .expect(200,done);// O status da requisição for 200 e a função done é usada para que 
+            // a próxima instrução só seja executada quando o callback for completamente executado.
     });
 });
