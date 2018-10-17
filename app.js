f8fc2fa9e15f6eb361401a033bfe69cd45acdd9a
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-10-17 09:13:43 -0300

    Setando o objeto do WebSocket e criando o webSocket na rota de cadastro de promoções

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // e vai retornar a função do module.exports. Após o caminho do require, já invocamos
 // a função retornada de module.exports
 const app = require('./config/express')();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.listen(3000, () => {
+// Criando o nome "io" e o valor dela é o objeto do WebSocket.
+app.set('io',io);
+
+http.listen(3000, () => {
     console.log('Servidor rodando...');
 });
--- app/routes/promocoes.js
+++ app/routes/promocoes.js
@@ -3,15 +3,17 @@ module.exports = app => {
         const connection = app.infra.connectionFactory();
         const produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.lista((erros, resultados) => {
-            if(erros){
+            if (erros) {
                 return next(erros);
             }
-            resp.render("promocoes/form", {lista : resultados});
+            resp.render("promocoes/form", { lista: resultados });
         });
     });
 
     app.post("/promocoes", (req, resp) => {
-        let produto = req.body;
+        let promocao = req.body;
+        // .get("param") retorna o objeto dentro da variável "io".
+        app.get("io").emit("novaPromocao", promocao); //Manda um aviso informando que ocorreu um evento associado ao nome novaPromocao e passa a varíavel para o client.
         resp.redirect("promocoes/form");
     });
 }
